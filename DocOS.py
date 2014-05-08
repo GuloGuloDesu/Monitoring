66b94d3e4097a9bc0b189c4d36e30f0634a2ffba
GuloGuloDesu <GuloGuloDesu@GMail.com>
2014-05-08 11:37:21 -0600

    Fixed the SQL query as it was missing a FROM and a column identifier, also fixed the iteritems dictionary error that is in Python 3

--- DocOS.py
+++ DocOS.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python3.3
 from PyFunc import *
 
 #Global variables are pulled from PyFunc.py
@@ -15,7 +16,8 @@ sql_ip_query = "SELECT "\
                "WHERE "\
                  "tblDocIP.DeviceName NOT IN ("\
                    "SELECT "\
-                     "tblDocOS "\
+                     "tblDocOS.DeviceName "\
+                   "FROM tblDocOS "\
                    "WHERE "\
                      "tblDocOS.DateStamp = CURDATE()"\
                    ") "\
@@ -25,53 +27,53 @@ sql_ip_query = "SELECT "\
                        "MAX(DateStamp) "\
                      "FROM tblDocIP"\
                    ")"
-sql_ip_results = sql_query('Monitoring', qsql_ip_query, 'Read')
+sql_ip_results = sql_query('Monitoring', sql_ip_query, 'Read')
 
 ips_fqdns = {}
 for sql_ip_result in sql_ip_results:
-	query_ip = sql_ip_result[1]
-	query_fqdn = sql_ip_result[0]
-	ips_fqdns[query_ip] = query_fqdn
+    query_ip = sql_ip_result[1]
+    query_fqdn = sql_ip_result[0]
+    ips_fqdns[query_ip] = query_fqdn
 
 #Assign a total size for the progress bar
 progress_bar = ProgressBar(
-	len(ips_fqdns), max_part, current_part
+    len(ips_fqdns), max_part, current_part
 )
 current_part = current_part + 1
 progress_bar.draw()
 
 fqdns_scans_raw = {}
-for ip, fqdn in ips_fqdns.iteritems():
-	progress_bar.step()
-	#Verify that the IP Address is online then try to pull the OS from SNMP
-	ping_status = verify_online_ping(ip)
-	if ping_status == "Success":
-		snmp_os_get = snmp_get(
-				2, "1", "", ip, ".1.3.6.1.2.1.1.1.0"
-		)
-		try:
-			snmp_os = (snmp_oid(
-					snmp_os_get, -1, "= STRING:")[1]
-			)
-			fqdns_scans_raw[ip] = (fqdn, "snmp", snmp_os)
-		except TypeError:
-			pass
-	else:
-		fqdns_scans_raw[ip] = (fqdn, "ping", "Unknown")
+for ip in ips_fqdns:
+    progress_bar.step()
+    #Verify that the IP Address is online then try to pull the OS from SNMP
+    ping_status = verify_online_ping(ip)
+    if ping_status == "Success":
+        snmp_os_get = snmp_get(
+                2, "1", "", ip, ".1.3.6.1.2.1.1.1.0"
+        )
+        try:
+            snmp_os = (snmp_oid(
+                    snmp_os_get, -1, "= STRING:")[1]
+            )
+            fqdns_scans_raw[ip] = (ips_fqdns[ip], "snmp", snmp_os)
+        except TypeError:
+            pass
+    else:
+        fqdns_scans_raw[ip] = (ips_fqdns[ip], "ping", "Unknown")
 progress_bar.end()
 
 #Remove any IP addresses that the OS was pulled from SNMP
 for ip in fqdns_scans_raw:
-	ips_fqdns.pop(ip)
-	
+    ips_fqdns.pop(ip)
+    
 #Try to pull the OS from NMAP
 fqdns_nmap_raw = {}
 nmap_command = ("sudo nmap %s -O -n -oX nmap_os.xml" 
-		        % ", ".join(ips_fqdns)
+                % ", ".join(ips_fqdns)
 )
 nmap_os_scan = subprocess.Popen(
-		nmap_command, stdin=None, stdout=-1, 
-		stderr=-1, shell=True
+        nmap_command, stdin=None, stdout=-1, 
+        stderr=-1, shell=True
 )
 nmap_os_scan_wait = nmap_os_scan.communicate()
 
@@ -80,185 +82,185 @@ xml_root = xml_tree.getroot()
 
 #Assign a total size for the progress bar
 progress_bar = ProgressBar(
-	len(xml_root.findall("host")), max_part, current_part
+    len(xml_root.findall("host")), max_part, current_part
 )
 current_part = current_part + 1
 progress_bar.draw()
 
 for xml_host in xml_root.findall("host"):
-	progress_bar.step()
-	nmap_os = "Unknown"
-	if xml_host.find("address").attrib["addrtype"] == "ipv4":
-		nmap_ip = xml_host.find("address").attrib["addr"]
-	try:
-		if xml_host.find("./os/osmatch").attrib["accuracy"] == "100":
-			nmap_os = xml_host.find("./os/osmatch").attrib["name"]
-	except AttributeError:
-		fqdns_nmap_raw[nmap_ip] = (
-				ips_fqdns[nmap_ip], "nmap", nmap_os
-		)
-	fqdns_nmap_raw[nmap_ip] = (
-			ips_fqdns[nmap_ip], "nmap", nmap_os
-	)
+    progress_bar.step()
+    nmap_os = "Unknown"
+    if xml_host.find("address").attrib["addrtype"] == "ipv4":
+        nmap_ip = xml_host.find("address").attrib["addr"]
+    try:
+        if xml_host.find("./os/osmatch").attrib["accuracy"] == "100":
+            nmap_os = xml_host.find("./os/osmatch").attrib["name"]
+    except AttributeError:
+        fqdns_nmap_raw[nmap_ip] = (
+                ips_fqdns[nmap_ip], "nmap", nmap_os
+        )
+    fqdns_nmap_raw[nmap_ip] = (
+            ips_fqdns[nmap_ip], "nmap", nmap_os
+    )
 
 os.remove("nmap_os.xml")
 progress_bar.end()
 
 os_parsed = {
-		"Windows 2000 Version 5.0": "Windows Server 2000", 
-		"Windows Version 5.2": "Windows Server 2003", 
-		"Windows 2000 Version 5.1": "Windows XP Professional", 
-		"Microsoft Windows": "Windows Unknown", 
-		"Windows Version 6.1": "Windows Check", 
-		"Windows Version 6.0": "Windows Server 2008", 
-		"JETDIRECT": "HP Printer", 
-		"HP ETHERNET": "HP Printer", 
-		"Zebra": "Zebra Printer", 
-		"Avaya Phone": "Avaya Phone", 
-		"Cisco IOS Software": "Cisco Check", 
-		"Ethernet Switch with PoE": "Cisco PoE Switch", 
-		"Avaya S8300 Server": "Avaya Phone Server", 
-		"Raritan Dominion PX": "Raritan Dominion PX", 
-		"DKX2": "Raritan KVM", 
-		"Xerox WorkCentre": "Xerox Printer", 
-		"Tape Library": "Tape Library", 
-		"NetApp": "NetApp SAN", 
-		"Linux": "Linux", 
-		"Unknown": "Unknown"
-	}
+        "Windows 2000 Version 5.0": "Windows Server 2000", 
+        "Windows Version 5.2": "Windows Server 2003", 
+        "Windows 2000 Version 5.1": "Windows XP Professional", 
+        "Microsoft Windows": "Windows Unknown", 
+        "Windows Version 6.1": "Windows Check", 
+        "Windows Version 6.0": "Windows Server 2008", 
+        "JETDIRECT": "HP Printer", 
+        "HP ETHERNET": "HP Printer", 
+        "Zebra": "Zebra Printer", 
+        "Avaya Phone": "Avaya Phone", 
+        "Cisco IOS Software": "Cisco Check", 
+        "Ethernet Switch with PoE": "Cisco PoE Switch", 
+        "Avaya S8300 Server": "Avaya Phone Server", 
+        "Raritan Dominion PX": "Raritan Dominion PX", 
+        "DKX2": "Raritan KVM", 
+        "Xerox WorkCentre": "Xerox Printer", 
+        "Tape Library": "Tape Library", 
+        "NetApp": "NetApp SAN", 
+        "Linux": "Linux", 
+        "Unknown": "Unknown"
+    }
 
 #Assign a total size for the progress bar
 progress_bar = ProgressBar(
-	len(fqdns_scans_raw), max_part, current_part
+    len(fqdns_scans_raw), max_part, current_part
 )
 current_part = current_part + 1
 progress_bar.draw()
 
 fqdns_scan_os_parsed = {}
 for ip, fqdns_scans_oss in fqdns_scans_raw.iteritems():
-	progress_bar.step()
-	os = fqdns_scans_oss[2]
-	fqdns = fqdns_scans_oss[0]
-	scan_type = fqdns_scans_oss[1]
-	not_in_os_parsed = 0
-	
-	for raw, parsed in os_parsed.iteritems():
-		if raw in os:
-			if parsed == "Windows Check":
-				win_auth = funcUserPass()
-				win_cmd = (
-						"/usr/local/wmi/bin/wmic -U %s%%%s \
-						//%s 'SELECT OperatingSystemSKU \
-						FROM Win32_OperatingSystem'"
-						%(win_auth[0], win_auth[1], ip)
-				)
-				win_os_scan = subprocess.Popen(
-						win_cmd, stdin=None, stdout=-1, 
-						stderr=-1, shell=True
-				)
-				while True:
-					read_line = win_os_scan.stdout.readline()
-					if not read_line: break
-					if "'CP850' unavailable" not in read_line and "48" in read_line:
-						fqdns_scan_os_parsed[ip] = (
-							fqdns, scan_type, "Windows 7 Professional"
-						)
-					elif "'CP850' unavailable" not in read_line and "8" in read_line:
-						fqdns_scan_os_parsed[ip] = (
-							fqdns, scan_type, "Windows Server 2008"
-						)
-					elif "'CP850' unavailable" not in read_line and "7" in read_line:
-						fqdns_scan_os_parsed[ip] = (
-							fqdns, scan_type, "Windows Server 2008"
-						)
-					else:
-						fqdns_scan_os_parsed[ip] = (
-							fqdns, scan_type, "Unknown"
-						)
-				not_in_os_parsed = 1
-				break
-			elif parsed == "Cisco Check":
-				if "C1200" in os or "C1240" in os or "C1310" in os:
-					fqdns_scan_os_parsed[ip] = (
-							fqdns, scan_type, "Cisco WAP"
-					)
-				else:
-					fqdns_scan_os_parsed[ip] = (
-							fqdns, scan_type, "Cisco Switch"
-					)
-				not_in_os_parsed = 1
-				break
-			else:
-				fqdns_scan_os_parsed[ip] = (
-						fqdns, scan_type, parsed
-				)
-				not_in_os_parsed = 1
-				break
-	if not_in_os_parsed == 0:
-		fqdns_scan_os_parsed[ip] = (
-				fqdns, scan_type, "Unknown"
-		)
+    progress_bar.step()
+    os = fqdns_scans_oss[2]
+    fqdns = fqdns_scans_oss[0]
+    scan_type = fqdns_scans_oss[1]
+    not_in_os_parsed = 0
+    
+    for raw, parsed in os_parsed.iteritems():
+        if raw in os:
+            if parsed == "Windows Check":
+                win_auth = funcUserPass()
+                win_cmd = (
+                        "/usr/local/wmi/bin/wmic -U %s%%%s \
+                        //%s 'SELECT OperatingSystemSKU \
+                        FROM Win32_OperatingSystem'"
+                        %(win_auth[0], win_auth[1], ip)
+                )
+                win_os_scan = subprocess.Popen(
+                        win_cmd, stdin=None, stdout=-1, 
+                        stderr=-1, shell=True
+                )
+                while True:
+                    read_line = win_os_scan.stdout.readline()
+                    if not read_line: break
+                    if "'CP850' unavailable" not in read_line and "48" in read_line:
+                        fqdns_scan_os_parsed[ip] = (
+                            fqdns, scan_type, "Windows 7 Professional"
+                        )
+                    elif "'CP850' unavailable" not in read_line and "8" in read_line:
+                        fqdns_scan_os_parsed[ip] = (
+                            fqdns, scan_type, "Windows Server 2008"
+                        )
+                    elif "'CP850' unavailable" not in read_line and "7" in read_line:
+                        fqdns_scan_os_parsed[ip] = (
+                            fqdns, scan_type, "Windows Server 2008"
+                        )
+                    else:
+                        fqdns_scan_os_parsed[ip] = (
+                            fqdns, scan_type, "Unknown"
+                        )
+                not_in_os_parsed = 1
+                break
+            elif parsed == "Cisco Check":
+                if "C1200" in os or "C1240" in os or "C1310" in os:
+                    fqdns_scan_os_parsed[ip] = (
+                            fqdns, scan_type, "Cisco WAP"
+                    )
+                else:
+                    fqdns_scan_os_parsed[ip] = (
+                            fqdns, scan_type, "Cisco Switch"
+                    )
+                not_in_os_parsed = 1
+                break
+            else:
+                fqdns_scan_os_parsed[ip] = (
+                        fqdns, scan_type, parsed
+                )
+                not_in_os_parsed = 1
+                break
+    if not_in_os_parsed == 0:
+        fqdns_scan_os_parsed[ip] = (
+                fqdns, scan_type, "Unknown"
+        )
 progress_bar.end()
-		
+        
 
 #Assign a total size for the progress bar
 progress_bar = ProgressBar(
-	len(fqdns_nmap_raw), max_part, current_part
+    len(fqdns_nmap_raw), max_part, current_part
 )
 current_part = current_part + 1
 progress_bar.draw()
-			
+            
 for ip, fqdns_nmap_raws in fqdns_nmap_raw.iteritems():
-	progress_bar.step()
-	os = fqdns_nmap_raws[2]
-	fqdns = fqdns_nmap_raws[0]
-	scan_type = fqdns_nmap_raws[1]
+    progress_bar.step()
+    os = fqdns_nmap_raws[2]
+    fqdns = fqdns_nmap_raws[0]
+    scan_type = fqdns_nmap_raws[1]
 
-	for raw, parsed in os_parsed.iteritems():
-		if raw in os:
-			fqdns_scan_os_parsed[ip] = (
-					fqdns, scan_type, parsed
-			)
-			break
-		else:
-			fqdns_scan_os_parsed[ip] = (
-					fqdns, scan_type, "Unknown"
-			)
+    for raw, parsed in os_parsed.iteritems():
+        if raw in os:
+            fqdns_scan_os_parsed[ip] = (
+                    fqdns, scan_type, parsed
+            )
+            break
+        else:
+            fqdns_scan_os_parsed[ip] = (
+                    fqdns, scan_type, "Unknown"
+            )
 progress_bar.end()
-			
+            
 #Assign a total size for the progress bar
 progress_bar = ProgressBar(
-	len(fqdns_scan_os_parsed), max_part, current_part
+    len(fqdns_scan_os_parsed), max_part, current_part
 )
 current_part = current_part + 1
 progress_bar.draw()
 for ip, fqdn_scan_os in fqdns_scan_os_parsed.iteritems():
-	#~ progress_bar.step()
-	fqdn = fqdn_scan_os[0]
-	scan_type = fqdn_scan_os[1]
-	os = fqdn_scan_os[2]
-	
-	sql_insert = "INSERT INTO tblDocOS \
-			(DeviceName, OS, ScanType, DateStamp) \
-			VALUES('%s', '%s', '%s', CURDATE())" \
-			%(fqdn, os, scan_type)
-	
-	#Queue for insertion into the DB
-	#~ print sql_insert
-	sql_cursor.execute(sql_insert)
+    #~ progress_bar.step()
+    fqdn = fqdn_scan_os[0]
+    scan_type = fqdn_scan_os[1]
+    os = fqdn_scan_os[2]
+    
+    sql_insert = "INSERT INTO tblDocOS \
+            (DeviceName, OS, ScanType, DateStamp) \
+            VALUES('%s', '%s', '%s', CURDATE())" \
+            %(fqdn, os, scan_type)
+    
+    #Queue for insertion into the DB
+    #~ print sql_insert
+    sql_cursor.execute(sql_insert)
 progress_bar.end()
-		
+        
 #Insert into DocScripts the approximate run time
 sql_cursor.execute("INSERT INTO tblDocScripts (ScriptName, RunTime, DateStamp) \
-	VALUES('%s', '%s', CURDATE())" \
-	%(script_name, str(time.mktime(time.localtime()) - time.mktime(start_time))))
-	
+    VALUES('%s', '%s', CURDATE())" \
+    %(script_name, str(time.mktime(time.localtime()) - time.mktime(start_time))))
+    
 #Run the SQL Update query and close the conection
 sql_connection.commit()
 sql_connection.close
 
 #Call function to display the script run time
-print funcPythonRunTime(time.localtime())
+print(funcPythonRunTime(time.localtime()))
 
 #Clear all variables the script has used
 locals().clear()
