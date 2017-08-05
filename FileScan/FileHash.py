db6af43ee31c2094b1e9926192f66e64b7b141e0
GuloGuloDesu <GuloGuloDesu@Gmail.com>
2017-08-04 19:40:52 -0700

    Wrote more of the script, still need to test

--- FileScan/FileHash.py
+++ FileScan/FileHash.py
@@ -16,4 +16,15 @@ class FileScan(theading.Thread):
         threading.Thread.__init__(self)
         self.file_correct_path = file_correct_path
     def run(self):
-        
+            if len(file_correct_path) < 255: #DB Field size is 255
+                try:
+                    #Pull created, modified dates of file
+                    file_info = os.stat(os.path.join(root, name))
+                    md5_hash = md5.new()
+                    #Open file in binary mode to calculate md5 of entire file 
+                    file_open = file(os.path.join(root, name), 'rb')
+                    while True:
+                        file_chunk = file_open.read(1024)
+                        if len(file_chunk) == 0: break
+                        md5_hash.update(file_chunk)
+                    file_hash = md5_hash.hexdigest()
